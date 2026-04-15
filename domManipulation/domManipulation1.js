586721a8521e6dcc0a0948f2837ca9b7c1794041
Pranab Sethi <pranab2108.codes@gmail.com>
2026-04-15 23:11:09 +0530

    domManipulation.jsx

--- domManipulation/domManipulation1.js
+++
@@ -1,9 +0,0 @@
-function onButtonPress() {
-
-    const currentValue = document.getElementById("btn").innerHTML;
-    console.log(currentValue.split(" "))
-    const currentCounter = currentValue.split(" ")[1];
-    const newCounter = parseInt(currentCounter) + 1;                                         /* Here we are converting the 1th index element which is a character into int so that's why we are using parseInt. */
-    document.getElementById("btn").innerHTML = "Counter " + newCounter;
-
-}
