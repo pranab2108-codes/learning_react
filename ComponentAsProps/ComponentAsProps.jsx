112e44b4cfcd4df559801b6803d274d67d47b617
Pranab2108-Codes <pranab2108.codes@gmail.com>
2026-04-21 23:28:11 +0530

    ComponentAsProps.jsx

---
+++ ComponentAsProps/ComponentAsProps.jsx
@@ -0,0 +1,54 @@
+function ComponentAsProps(){
+                                                                     /* Here we are passing the component as input/props to another component. */
+    return <div>
+
+        <Wrapper1 innerComponent={<TextComponent />}></Wrapper1>
+        <Wrapper2><div>Hi There</div></Wrapper2>
+        <Wrapper2><Wrapper1 innerComponent={<TextComponent />}></Wrapper1></Wrapper2>
+    
+    </div>
+                                                                     /* After the "Wrapper2" whatever written in there all those can be consider as a child, a child can be either a component or an XML expression also. */
+}
+
+function TextComponent(){
+
+    return <div>
+
+        Hey people.
+
+    </div>
+
+}
+
+function Wrapper1({innerComponent}){
+
+    return <div style={{border:"2px solid black", padding:"20px"}}>
+
+    {innerComponent}
+
+    </div>
+
+}
+
+function Wrapper2({children}){
+
+    return <div style={{border:"2px solid red", padding:"20px"}}>
+
+    {children}
+
+    </div>
+
+}
+
+export default ComponentAsProps;
+
+
+// import { StrictMode } from 'react'                                /* This should be writte inside of src/main.jsx. */
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import ComponentAsProps from '../ComponentAsProps/ComponentAsProps.jsx';
+// createRoot(document.getElementById('root')).render(
+  
+//     <ComponentAsProps />
+    
+// );
