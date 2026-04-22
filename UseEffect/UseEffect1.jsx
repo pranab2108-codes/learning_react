2eb0dd4231ad365af22f682483af632bf0116147
Pranab2108-Codes <pranab2108.codes@gmail.com>
2026-04-23 00:23:37 +0530

    UseEffect.jsx

---
+++ UseEffect/UseEffect1.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { useEffect } from "react";                                        /* The useState allow us to perform side effects in function components, side effects are the operations that can affect other components or can't be done during rendering, such as fetching, subscription, or manually changing the DOM in React components. */
+
+
+function UseEffect1() {                                                   /* If we fetch something from any site and we do rendering that, then it will act as a loop, it will keep rerender the application and do fetch each time. */
+
+  const [todos, setTodos] = useState({});
+
+  useEffect(() => {
+
+    fetch("https://sum-server.100xdevs.com/todos")
+      .then(async function (res) {
+
+        const json = await res.json();
+        setTodos(json.todos);                                             /* Whenever the setTodos get called this application will again rerender if we would haven't used this useEffect then the fetch call would have called again which is unneccessary, and it's making the whole situation look like a loop. */
+
+      });                                                                 /* In a F1 car race there are 100laps but the car need to stop in pit stop under some condition only, not all the time unneccessarily. */
+
+  }, []);                                                                 /* This empty array is being called as dependency array, it says this fetch call will only render once and not rerender again at all, so basically it is define for when should the callback function run, it only take input as state variable. */
+
+  return (
+
+    <div>
+
+      {todos.map((todo) => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
+    
+    </div>
+
+  );
+
+}
+
+function Todo(){
+
+    return <div>
+
+        <h1>{title}</h1>
+        {description}
+
+    </div>
+
+}
+
+export default UseEffect1;
+
+
+// import { StrictMode } from 'react'                                     /* It should be written in src/main.jsx. */
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import UseEffect1 from '../UseEffect/UseEffect1.jsx';
+// createRoot(document.getElementById('root')).render(
+  
+//     <UseEffect1 />
+    
+// );
---
+++ UseEffect/UseEffect2.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
+
+function UseEffect2() {
+  
+  const [selectedId, setSelectedId] = useState(0);
+                                                                 /* When the first button get clicked the id or selectedId become 1, then for second button it become 2, and so on. */
+  return (
+
+    <div>
+
+      <button onClick={function () {
+
+          setSelectedId(1);
+
+        }}
+      >Button 1
+      </button>
+
+      <button onClick={function () {
+
+          setSelectedId(2);
+
+        }}
+      >Button 2
+      </button>
+
+      <button onClick={function () {
+
+          setSelectedId(3);
+
+        }}
+      >Button 3
+      </button>
+
+      <Todo id={selectedId} />
+
+    </div>
+
+  );
+
+}
+
+function Todo({ id }) {
+
+  const [todo, setTodo] = useState({});
+
+  useEffect(() => {
+
+    axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)
+      .then(function (response) {
+
+        setTodo(response.data.todo);
+
+    });
+
+  }, [id]);                                                      /* Here it means if any button get clicked means id will change, so the endpoint/URL going to hit again which makes it fetch new todo, but if we would have not given the id means let it be an empty array, then doesn't matter how many times we click any button among thse three, the todo will never get change means fetch will run only one time not anymore at all again. */
+
+  return ( 
+  
+    <div>
+
+        Id: {id}
+        <h1>{todo.title}</h1>
+        <h4>{todo.description}</h4>
+
+    </div>
+
+    );
+
+}
+
+export default UseEffect2;
+
+
+// import { StrictMode } from 'react'                            /* It should be written in src/main.jsx. */
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import UseEffect2 from '../UseEffect/UseEffect2.jsx';
+// createRoot(document.getElementById('root')).render(
+  
+//     <UseEffect2 />
+    
+// );
