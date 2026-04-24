0298e4803dba50b59586ce8c985bab973da99e36
Pranab2108-Codes <pranab2108.codes@gmail.com>
2026-04-24 11:15:05 +0530

    CustomHook.jsx

---
+++ CustomHook/CustomHook.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+
+function useTodos() {                                        /* The customHooks are always start with "use" word. */
+                                                             /* The custom hooks are only can be called either from component or by other custom hooks. */
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+
+    axios.get("https://sum-server.100xdevs.com/todos")
+        .then((res) => {
+
+            setTodos(res.data.todos);
+
+        });
+
+  }, []);
+
+  return todos;
+
+}
+
+function CustomHook() {
+
+  const todos = useTodos();
+
+  return (
+
+    <div>
+
+      {todos}
+
+    </div>
+
+  );
+
+}
+
+export default CustomHook;
+
+
+// import { StrictMode } from 'react'                        /* It should be written inside src/main.jsx. */
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import CustomHook from '../CustomHook/CustomHook.jsx';
+// createRoot(document.getElementById('root')).render(
+  
+//     <CustomHook/>
+    
+// );
