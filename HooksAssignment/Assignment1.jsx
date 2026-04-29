ab9d1cf924ec6dd48c1a7959bf86192ab98c2e30
Pranab2108-Codes <pranab2108.codes@gmail.com>
2026-04-29 23:33:44 +0530

    HooksAssignment.jsx

---
+++ HooksAssignment/Assignment1.jsx
@@ -0,0 +1,43 @@
+import { useState,useMemo } from "react";
+
+export function Assignment1() {
+
+  const [input, setInput] = useState(0);
+
+  const expensiveValue = useMemo(() => {
+
+    let value = 1;
+    for (let i = 1; i <= input; i++) {
+
+        value *= i;
+
+    }
+    return value;
+    
+  },[input]);                                                                /* When this input will change, then only this expensive/costly function of calculating factorial will run. */
+  
+  return (
+
+    <div>
+
+      <input type="number" onChange={(event) => setInput(Number(event.target.value))}/>
+      <p>Calculated factorial is: {expensiveValue}</p>
+
+    </div>
+
+  );
+
+}
+
+export default Assignment1;
+
+
+// import { StrictMode } from 'react'                                          /* It should be written in src/main.jsx. */
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import Assignment1 from '../HooksAssignment/Assignment1.jsx';
+// createRoot(document.getElementById('root')).render(
+  
+//     <Assignment1/>
+    
+// );
