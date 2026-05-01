8af0903b5e0c3a959c7d4b9c6120490b8b747dab
Pranab2108-Codes <pranab2108.codes@gmail.com>
2026-05-01 23:06:27 +0530

    HooksAssignment.jsx

---
+++ HooksAssignment/Assignment3.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useMemo } from 'react';
+
+
+export const Assignment3 = () => {
+
+    const [items, setItems] = useState([
+
+        { name: 'Chocolates', value: 10 },
+        { name: 'Chips', value: 20 },
+        { name: 'Onion', value: 30 },
+        { name: 'Tomato', value: 30 },
+        
+    ]);
+
+    const totalValue = useMemo( () =>{
+
+        let value = 0;
+        for (let i = 0; i < items.length; i++) {
+
+            value = value + items[i].value;
+
+        }
+        return value;
+
+    },[items]);                                                                /* Whenever the items getting changes, then only this costly opration should run. */
+
+    return (
+
+        <div>
+
+            <ul>
+
+                {items.map((item, index) => (
+
+                    <li key={index}>{item.name} - Price: ${item.value}</li>
+                
+                ))}
+
+            </ul>
+            <p>Total Value: {totalValue}</p>
+
+        </div>
+
+    );
+
+};
+
+
+// import { StrictMode } from 'react'                                          /* It should be written in src/main.jsx. */
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import { Assignment3 } from '../HooksAssignment/Assignment3.jsx';
+// createRoot(document.getElementById('root')).render(
+  
+//     <Assignment3/>
+    
+// );
