53a5511f084eac8ab0a9a479c8dbeb5f3fa1bfd7
Pranab2108-Codes <pranab2108.codes@gmail.com>
2026-05-12 10:59:12 +0530

    TodoApplication.jsx

--- TodoApplication/TodoApplication2And3.jsx
+++ TodoApplication/TodoApplication2And3.jsx
@@ -1,141 +1,145 @@
 import { useState } from "react";
 
 
-function TodoApplication2(){
+function TodoApplication2() {
 
-    const [todos2, setTodos2] = useState([{
+  const [todos2, setTodos2] = useState([{
 
-        title: "Goto Gym",
-        description: "From 6:00 AM - 8:00 AM",
-        completed: false,
+    title: "Goto Gym",
+    description: "From 6:00 AM - 8:00 AM",
+    completed: false,
 
-    },{
+  },{
 
-        title: "Goto Study DSA",
-        description: "From 9:00 AM - 12:00 PM",
-        completed: true,
+    title: "Goto Study DSA",
+    description: "From 9:00 AM - 12:00 PM",
+    completed: true,
 
-    }]);
+  }]);
 
-    function addTodo2(){
+  function addTodo2() {
 
-        let newTodos = [];
+    let newTodos = [];
 
-        for(let i=0;i<todos2.length;i++){
+    for(let i = 0; i < todos2.length; i++){
 
-            newTodos.push(todos2[i]);
-
-        }
-        newTodos.push({
-
-            title: "Hey",
-            description: "I am just checking"
-
-        });
-        setTodos2(newTodos);
+      newTodos.push(todos2[i]);
 
     }
+    newTodos.push({
 
-    return (
+      title: "Hey",
+      description: "I am just checking"
 
-        <div>       
+    });
+    setTodos2(newTodos);
 
-            <Todo2 title= "Pranab" description="Software Engineer"> </Todo2>                                                         
-            <Todo2 title= "Deepak" description="Software Engineer"> </Todo2>
-            <Todo2 title= "Souvik" description="Sr. Developer"> </Todo2>
-            <Todo2 title={todos2[0].title} description={todos2[0].description}></Todo2>                       
-            <Todo2 title={todos2[1].title} description={todos2[1].description}></Todo2>
-            {todos2[2] ? (<Todo2 title={todos2[2].title} description={todos2[2].description} />) : null}            
-            <button onClick={addTodo2}>Adding a random Todo</button>
+  }
 
-        </div>
+  return (
 
-    );
+    <div>       
+
+      <Todo2 title="Pranab" description="Software Engineer"> </Todo2>                                                         
+      <Todo2 title="Deepak" description="Software Engineer"> </Todo2>
+      <Todo2 title="Souvik" description="Sr. Developer"> </Todo2>
+      <Todo2 title={todos2[0].title} description={todos2[0].description}></Todo2>                       
+      <Todo2 title={todos2[1].title} description={todos2[1].description}></Todo2>
+      {todos2[2] ? (<Todo2 title={todos2[2].title} description={todos2[2].description} />) : null}            
+      <button onClick={addTodo2}>Adding a random todo</button>
+
+    </div>
+
+  );
 
 }
 
-function Todo2(props){
+function Todo2(props) {
 
-    return <div style={{background: "blue"}}>
+  return <div style={{background: "blue"}}>
 
-        <h1 style={{color: "red"}}>{props.title} ,{props.description}</h1>
+            <h1 style={{color: "red"}}>{props.title}, {props.description}</h1>
 
-    </div>
+         </div>
 
 }
     
 
-function TodoApplication3(){
+function TodoApplication3() {
 
-    const [todos3, setTodos3] = useState([{
+  const [todos3, setTodos3] = useState([{
 
-        title: "Goto Kitchen",
-        description: "From 12:00 PM - 2:00 PM",
-        completed: true,
+    title: "Goto Kitchen",
+    description: "From 12:00 PM - 2:00 PM",
+    completed: true,
 
-    },{
+  },{
 
-        title: "Goto Shop Market",
-        description: "From 5:00 PM - 6:00 PM",
-        completed: true,
+    title: "Goto Shop Market",
+    description: "From 5:00 PM - 6:00 PM",
+    completed: true,
+
+  }]);
+
+  function addTodo3() {
+        
+    setTodos3([...todos3, {
+
+      title: "Now study CP",
+      description: "From TLE Eliminators"
+
+    }]);
+    setTodos3([...todos3, {           
+                                                                                                      /* Here we are doing setTodos again below of one setTodos so order matters here, now only the 2nd one get render each time. */
+      title: "Now time for System Design",
+      description: "Learn it from Coder Army "
 
     }]);
 
-    function addTodo3(){
-        
-        setTodos3([...todos3, {
+  }
 
-            title: "Now study CP",
-            description: "From TLE Eliminators"
+  return (
 
-        }]);
-        setTodos3([...todos3,{           
-                                                                                                      /* Here we are doing setTodos again below of one setTodos so order matters here, now only the 2nd one get render each time. */
-            title: "Now time for System Design",
-            description: "Learn it from Coder Army "
+    <div>           
 
-        }]);
+      {todos3.map(function(todo) {
 
-    }
+        return <Todo3 title={todo.title} description={todo.description}></Todo3>
 
-    return (
-
-        <div>           
-
-            {todos3.map(function(todo){
-
-                return <Todo3 title={todo.title} description={todo.description}></Todo3>
-
-            })}
-            <button onClick={addTodo3}>Adding a random Todo</button>
+      })}
+      <button onClick={addTodo3}>Adding a random todo</button>
             
-        </div>
-    )
+    </div>
+    
+  )
 
 }
 
-function Todo3(props){
+function Todo3(props) {
 
-    return <div style={{background: "green"}}>
+  return <div style={{background: "green"}}>
 
-        <h1 style={{color: "aquamarine"}}>{props.title} ,{props.description}</h1>
+            <h1 style={{color: "aquamarine"}}>{props.title}, {props.description}</h1>
 
-    </div>
+         </div>
 
 }
 
 export { TodoApplication2 , TodoApplication3 };                                                       /* When there will be more than 1 functions to export we shouldn't use default. */
 
 
-// import { StrictMode } from 'react';
+// import { StrictMode } from 'react';                                                                /* It should be written inside src/main.jsx. */
 // import { createRoot } from 'react-dom/client';
 // import './index.css';
 // import  { TodoApplication2, TodoApplication3 } from '../TodoApplication/TodoApplication2And3';
+
 // createRoot(document.getElementById('root')).render(
   
-//     <> 
+//   <> 
+
 //     <TodoApplication2 />
 //     <TodoApplication3 />
-//     </>
+
+//   </>
 
 // );
--- TodoApplication/todoApplication.js
+++ TodoApplication/todoApplication.js
@@ -5,160 +5,160 @@ let oldTodoState = [];
 
 function createChild(title, description, id) {
 
-    const child = document.createElement("div");
-    const firstGrandChild = document.createElement("div");
-    firstGrandChild.innerHTML = title;
-    const secondGrandChild = document.createElement("div");
-    secondGrandChild.innerHTML = description;
-    const thirdGrandChild = document.createElement("button");
-    thirdGrandChild.innerHTML = "Mark as done";
-    thirdGrandChild.setAttribute("onclick", `markAsDone(${id})`);
-    child.appendChild(firstGrandChild);
-    child.appendChild(secondGrandChild);
-    child.appendChild(thirdGrandChild);
-    child.setAttribute("id", id);
-    return child;
+  const child = document.createElement("div");
+  const firstGrandChild = document.createElement("div");
+  firstGrandChild.innerHTML = title;
+  const secondGrandChild = document.createElement("div");
+  secondGrandChild.innerHTML = description;
+  const thirdGrandChild = document.createElement("button");
+  thirdGrandChild.innerHTML = "Mark as done";
+  thirdGrandChild.setAttribute("onclick", `markAsDone(${id})`);
+  child.appendChild(firstGrandChild);
+  child.appendChild(secondGrandChild);
+  child.appendChild(thirdGrandChild);
+  child.setAttribute("id", id);
+  return child;
 
 }
 
 function markAsDone(id) {
 
-    const parent = document.getElementById(id);
-    parent.children[2].innerHTML = "Done!";
+  const parent = document.getElementById(id);
+  parent.children[2].innerHTML = "Done!";
 
 }
 
 
 function addTodoToDom(todo) {
 
-    const parent = document.getElementById("todos");
-    parent.appendChild(createChild(todo.title, todo.description, todo.id));
+  const parent = document.getElementById("todos");
+  parent.appendChild(createChild(todo.title, todo.description, todo.id));
 
 }
 
 function removeTodoFromDom(todo) {
 
-    const element = document.getElementById(todo.id);
-    element.parentNode.removeChild(element);
+  const element = document.getElementById(todo.id);
+  element.parentNode.removeChild(element);
 
 }
 
 function updateTodoInDom(oldTodo, newTodo) {
 
-    const element = document.getElementById(oldTodo.id);
-    element.children[0].innerHTML = newTodo.title;
-    element.children[1].innerHTML = newTodo.description;
+  const element = document.getElementById(oldTodo.id);
+  element.children[0].innerHTML = newTodo.title;
+  element.children[1].innerHTML = newTodo.description;
 
 }
 
 
 function updateState(newTodos) {
 
-    const added = [];
-    const deleted = [];
-    const updated = [];
+  const added = [];
+  const deleted = [];
+  const updated = [];
 
-    for (let i = 0; i < newTodos.length; i++) {                                             /* This loop will get trigger whenever a new todo get add in todoState. */
+  for(let i = 0; i < newTodos.length; i++) {                                             /* This loop will get trigger whenever a new todo get add in todoState. */
 
-        let found = false;
-        for (let j = 0; j < oldTodoState.length; j++) {
+    let found = false;
+    for(let j = 0; j < oldTodoState.length; j++) {
 
-            if (newTodos[i].id === oldTodoState[j].id) {
+      if(newTodos[i].id === oldTodoState[j].id) {
 
-                found = true;
+        found = true;
 
-            }
+      }
 
-        }
-        if (!found) {
+    }
+    if(!found) {
 
-            added.push(newTodos[i]);
-
-        }
+      added.push(newTodos[i]);
 
     }
 
-    for (let i = 0; i < oldTodoState.length; i++) {                                         /* Let suppose we have done pop() opeartion from todoSate then ofcourse it should be delete from oldTodoState also because then only we can make comparision between newTodos which is our todoState and oldTodoState, at that time this loop will get trigger. */
+  }
 
-        let found = false;
-        for (let j = 0; j < newTodos.length; j++) {
+  for(let i = 0; i < oldTodoState.length; i++) {                                         /* Let suppose we have done pop() opeartion from todoSate then ofcourse it should be delete from oldTodoState also because then only we can make comparision between newTodos which is our todoState and oldTodoState, at that time this loop will get trigger. */
 
-            if (oldTodoState[i].id === newTodos[j].id) {
+    let found = false;
+    for(let j = 0; j < newTodos.length; j++) {
 
-                found = true;
+      if(oldTodoState[i].id === newTodos[j].id) {
 
-            }
+        found = true;
 
-        }
-        if (!found) {
+      }
 
-            deleted.push(oldTodoState[i]);
+    }
+    if(!found) {
 
-        }
+      deleted.push(oldTodoState[i]);
 
     }
 
-    for (let i = 0; i < newTodos.length; i++) {                                             /* Let suppose we have changed the title or description of a particular todo then it should be updated in onlTodoState ofcourse then only comparision can be possible, at that time this loop will get trigger. */
+  }
 
-        for (let j = 0; j < oldTodoState.length; j++) {
+  for(let i = 0; i < newTodos.length; i++) {                                             /* Let suppose we have changed the title or description of a particular todo then it should be updated in onlTodoState ofcourse then only comparision can be possible, at that time this loop will get trigger. */
 
-            if (newTodos[i].id === oldTodoState[j].id) {
+    for(let j = 0; j < oldTodoState.length; j++) {
 
-                if (newTodos[i].title !== oldTodoState[j].title || newTodos[i].description !== oldTodoState[j].description) {
+      if(newTodos[i].id === oldTodoState[j].id) {
 
-                    updated.push({
+        if(newTodos[i].title !== oldTodoState[j].title || newTodos[i].description !== oldTodoState[j].description) {
 
-                        oldTodo: oldTodoState[j],
-                        newTodo: newTodos[i]
+          updated.push({
 
-                    });
+            oldTodo: oldTodoState[j],
+            newTodo: newTodos[i]
 
-                }
-
-            }
+          });
 
         }
 
-    }
-
-    for (let i = 0; i < added.length; i++) {                                                /* These 3 loops will run at that moment when make changes in todoState like if we add or remove or update then it should reflect in DOM also, so that's why we are storing in oldTodoState so we can know which needs to be add, delete and updated. */
-                                                                                            /* Here we are doing like this because we dont need to put our new list of todos again in DOM each time, means only those parts will change which need to be change. */
-        addTodoToDom(added[i]);
-
-    }                                                                                       
-
-    for (let i = 0; i < deleted.length; i++) {
-
-        removeTodoFromDom(deleted[i]);
+      }
 
     }
 
-    for (let i = 0; i < updated.length; i++) {
+  }
 
-        updateTodoInDom(
+  for(let i = 0; i < added.length; i++) {                                                /* These 3 loops will run at that moment when make changes in todoState like if we add or remove or update then it should reflect in DOM also, so that's why we are storing in oldTodoState so we can know which needs to be add, delete and updated. */
+                                                                                         /* Here we are doing like this because we dont need to put our new list of todos again in DOM each time, means only those parts will change which need to be change. */
+    addTodoToDom(added[i]);
 
-            updated[i].oldTodo,
-            updated[i].newTodo
+  }                                                                                       
 
-        );
+  for(let i = 0; i < deleted.length; i++) {
 
-    }
-    oldTodoState = [...newTodos];
+    removeTodoFromDom(deleted[i]);
+
+  }
+
+  for(let i = 0; i < updated.length; i++) {
+
+    updateTodoInDom(
+
+      updated[i].oldTodo,
+      updated[i].newTodo
+
+    );
+
+  }
+  oldTodoState = [...newTodos];
 
 }
 
 
 function addTodo() {
 
-    const title = document.getElementById("title").value;
-    const description = document.getElementById("description").value;
-    todoState.push({
+  const title = document.getElementById("title").value;
+  const description = document.getElementById("description").value;
+  todoState.push({
 
-        title: title,
-        description: description,
-        id: globalId++
+    title: title,
+    description: description,
+    id: globalId++
 
-    });
-    updateState(todoState);
+  });
+  updateState(todoState);
 
 }
