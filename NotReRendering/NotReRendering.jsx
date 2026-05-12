3d22702fadc88321c9cd3ea1c643d262737c899a
Pranab2108-Codes <pranab2108.codes@gmail.com>
2026-05-12 09:17:10 +0530

    NotReRendering.jsx

--- NotReRendering/NotReRendering.jsx
+++ NotReRendering/NotReRendering.jsx
@@ -5,13 +5,14 @@ import { useEffect } from "react";
 function NotReRendering() {
 
   let a = 1;
-  window.setInterval(() =>{
+
+  window.setInterval(() => {
 
     a++;
     console.log(a);                                                   /* Because of this we can see in console how the a is getting incrementing. */
 
-  },3000);
-
+  }, 3000);
+  
   return <DummyButton a={a}></DummyButton>                            /* Even after we pass the a to child component named as DummyButton it still can't re render because it is not a state in React and also we are not setting any updater function which would look after the state. */
 
 }
@@ -25,12 +26,13 @@ function DummyButton(props) {
 export default NotReRendering;
 
 
-// import { StrictMode } from 'react';
+// import { StrictMode } from 'react';                                /* I should be written inside src/main.jsx. */
 // import { createRoot } from 'react-dom/client';
 // import './index.css';
 // import NotReRendering from '../notReRendering/NotReRendering';
+
 // createRoot(document.getElementById('root')).render(
 
-//     <NotReRendering />
+//   <NotReRendering />
 
 // );
