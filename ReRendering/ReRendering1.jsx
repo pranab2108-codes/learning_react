8d0702e332e4c58061f0ef94cf66dd0c216ec717
Pranab2108-Codes <pranab2108.codes@gmail.com>
2026-05-12 09:24:58 +0530

    ReRendering.jsx

--- ReRendering/ReRendering1.jsx
+++ ReRendering/ReRendering1.jsx
@@ -1,33 +1,33 @@
 import { useState } from 'react';
 
 
-function ReRendering1(){                                                         /* Here the state variable is lies inside of parent that's why when the state change the parent(ReRendering1) is getting rerender, and because of it's returning 3 childrens which are 2 Header and 1 Button will get affected and rerender, even though we are not passing the state in this line "<Header title="My first name is Pranab"></Header>". */
+function ReRendering1() {                                                        /* Here the state variable is lies inside of parent that's why when the state change the parent(ReRendering1) is getting rerender, and because of it's returning 3 childrens which are 2 Header and 1 Button will get affected and rerender, even though we are not passing the state in this line "<Header title="My first name is Pranab"></Header>". */
 
-    const [title, setTitle] = useState("My last name is Sethi");                 /* Here we are updating the title or surname or last name through the setTitle. */
-                                                    
-    function updateTitle(){                 
+	const [title, setTitle] = useState("My last name is Sethi");                   /* Here we are updating the title or surname or last name through the setTitle. */
+																										
+	function updateTitle() {                 
 
-        setTitle("My last name is:" +Math.random());                             /* Any time we call this setTitle the whole app which is ReRendering here, would get rerender and irrespectively the childs will also rerender doesn't matter they are using the title which is a state variable here or not. */
+		setTitle("My last name is: " +Math.random());                                /* Any time we call this setTitle the whole app which is ReRendering here, would get rerender and irrespectively the childs will also rerender doesn't matter they are using the title which is a state variable here or not. */
 
-    }
-                                                                                 /* Here we only want the last name should be updated. */
-    return <>
-    
-        <Header title="My first name is Pranab"></Header>                         
-        <Header title={title}></Header>                                       
-        <button onClick={updateTitle}>Update the Title</button>
+	}
+																																								 /* Here we only want the last name should be updated, but unfortunately when button getting click the whole application ReRendering1 is rerendering so the first "Header" component which is holding the first name only, never getting updated also rerendering unneccessarily. */
+	return <>
+		
+					<Header title="My first name is Pranab"></Header>                         
+					<Header title={title}></Header>                                       
+					<button onClick={updateTitle}>Update the title</button>
 
-    </>
+				 </>                                                                     /* The main rerendering happening because of this updater function "updateTitle" which is use to update the state for state variable title. */
 
 }
 
-function Header({title}){
+function Header({ title }) {
 
-    return <div>
+	return <div>
 
-        {title}
+						{title}
 
-    </div>
+				 </div>
 
 }
 
@@ -38,8 +38,9 @@ export default ReRendering1;
 // import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import ReRendering1 from '../ReRendering/ReRendering1.jsx';
+
 // createRoot(document.getElementById('root')).render(
   
-//     <ReRendering1 />
-    
+//   <ReRendering1 />
+	
 // );
--- ReRendering/ReRendering2.jsx
+++ ReRendering/ReRendering2.jsx
@@ -1,55 +1,56 @@
 import { useState } from "react";
 
 
-function ReRendering2(){
-                                                                                          /* Because of this ReRendering2 doesn't have the state the Header with the first nae pass in it will not rerender and whenever this HeaderWithButton get rerender it's paret(ReRendering2) will get affected, so it won't rerender. */
-    return <div>
+function ReRendering2() {
+                                                                                          /* Because of this ReRendering2 doesn't have the state, so the Header with the first name passed in it will not rerender and whenever this HeaderWithButton get rerender it's paret(ReRendering2) will not get affected, so it(ReRendering2) won't rerender. */
+  return <div>
 
-        <Header title="My first name is Pranab"></Header>                                       
-        <HeaderWithButton></HeaderWithButton>
+            <Header title="My first name is Pranab"></Header>                                       
+            <HeaderWithButton></HeaderWithButton>
 
-    </div>
-
+         </div>                                                                           /* In "ReRendering1.jsx", we have seen the first name was unneccessarily rerendering, so here what we are exactly doing is putting the first name in there. where the state is not present, means the state is present only in there which suppose to be rerender, so the other things would not need to rerender. */
+                                                                                          /* Here React won't recreate this "<HeaderWithButton></HeaderWithButton>" because the state is not present in "ReRendering2". */
 }
 
-function HeaderWithButton(){
+function HeaderWithButton() {
 
-    const [title, setTitle] = useState("My last is Sethi");                                /* Here the state variable got pushed to the down component so if ever this the setTitle get called it will starts to rerender agai the HeaderWithButton and the Header which will pass with the last name and Button also get rerender because of it's child. */
+  const [title, setTitle] = useState("My last name is Sethi");                            /* Here the state variable got pushed to the down component so if ever this setTitle get called it will starts to rerender again the HeaderWithButton and the Header which will pass with the last name and Button also get rerender because of it's(HeaderWithButton()) child. */
  
-    function updateTitle(){
+  function updateTitle() {
 
-        setTitle("My last name is: " + Math.random());
+    setTitle("My last name is: " + Math.random());
 
-    }
+  }
 
-    return <div>
+  return <div>
 
-        <Header title={title}></Header>
-        <button onClick={updateTitle}>Click me to see update of Title</button>
+            <Header title={title}></Header>
+            <button onClick={updateTitle}>Click me to see update of title</button>
 
-    </div>
+         </div>
 
 }
 
-function Header(props){
+function Header(props) {
 
     return <div>
 
-        {props.title}
+              {props.title}
 
-    </div>
+           </div>
 
 }
 
 export default ReRendering2;
 
 
-// import { StrictMode } from 'react'                                                      /* It should be done in src/main.jsx. */
+// import { StrictMode } from 'react'                                                     /* It should be done in src/main.jsx. */
 // import { createRoot } from 'react-dom/client'
 // import './index.css'
 // import ReRendering2 from '../ReRendering/ReRendering2.jsx';
+
 // createRoot(document.getElementById('root')).render(
   
-//     <ReRendering2 />
+//   <ReRendering2 />
     
 // );
