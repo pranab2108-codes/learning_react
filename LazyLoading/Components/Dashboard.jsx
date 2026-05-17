23dad032f2337e1cf0a42e0649a7874d61dbc428
Pranab2108-Codes <pranab2108.codes@gmail.com>
2026-05-17 22:53:22 +0530

    LazyLoading.jsx

---
+++ LazyLoading/Components/Dashboard.jsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {                 /* Here we are using the default, which indicate this file need to export only one main component, not more than one. */
+
+  return <div>
+
+            Dashboard page
+  
+         </div>
+
+};
---
+++ LazyLoading/LazyLoading.jsx
@@ -0,0 +1,74 @@
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import {  lazy, Suspense } from 'react';
+const Dashboard = lazy(() => import('./Components/Dashboard.jsx'));                        /* Because of these Dashboard and Landing got export as default, that's why we are importing like this, we don't neeed to import like { Dashboard } and { Landing }, we can also rename here with anyname while import because of they got export as default. */
+const Landing = lazy(() => import('./Components/Landing.jsx'));
+
+                                                                                /* React always bundles the whole thing so when a prticular part will be needed later among them it can be use from there. */      
+function LazyLoading() {                                                        /* Let's suppose in our application there are many pages like Home, Dashboard, Settings, Messages, Profile. */
+                                                                                /* Normally React may bundle all pages together initially, browser downloads JavaScript for all pages, even though user only needs Home page right now, this makes initial loading heavier/slower. */                                                            
+  return (
+  
+      <div>
+  
+        <BrowserRouter> 
+  
+          <Appbar/>
+  
+          <Routes>
+  
+            <Route path="/dashboard" element={<Suspense fallback={<div>loading...</div>}><Dashboard/></Suspense>}></Route>
+            <Route path="/" element={<Suspense fallback={<div>loading...</div>}><Landing/></Suspense>}></Route>
+  
+          </Routes>
+  
+        </BrowserRouter>
+  
+      </div>
+  
+    );                                                                          /* With Lazy Loading react loads components only when route is visited. */                                                                        
+  
+  }
+  
+  function Appbar() {
+                                                                                /* Normally React bundles everything together, So even if the user only opens /, React still downloads Dashboard code, Settings code and all other pages codes also hence the bundle size get increase, but through lazy loading react loads components only when the user visits them. */
+    const navigate = useNavigate();                                             /* Let suppose the user open '/' so only Landing page code loads, then later user open /dashboard so Dashboard component downloads at that moment, it never means it will hit the backend server each time. */
+  
+    return <div>
+  
+        <div style={{backgroundColor: "green", color: "white"}}>
+  
+          Hi this is the topbar
+          <button onClick={() => {
+  
+              navigate("/");
+  
+            }}>Landing page
+  
+          </button>
+          <button onClick={() => {
+  
+              navigate("/dashboard");
+  
+            }}>Dashboard page
+  
+          </button>
+  
+        </div>
+  
+    </div>    
+                                                                              /* Now because of this we don't see any hard reloading. */
+}
+
+export default LazyLoading;
+
+
+// import { StrictMode } from 'react'                                         /* It should be written in src/main.jsx. */
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import LazyLoading from '../LazyLoading/LazyLoading';
+
+// createRoot(document.getElementById('root')).render(
+   
+//     <LazyLoading/>
+          
+// );
