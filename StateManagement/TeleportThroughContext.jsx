346068dee04ffdf39cd97fa97dac04e3791cea7f
Pranab2108-Codes <pranab2108.codes@gmail.com>
2026-05-21 23:24:00 +0530

    StateManagement.jsx

---
+++ StateManagement/TeleportThroughContext.jsx
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+
+export const CountContext = createContext(0);               /* It returns an object which is a (dot).Provider, now we created the context with an initial value of 0. */
