bcef8e5b1ad5166e42ad8c8342d740f8869e8388
Pranab2108-Codes <pranab2108.codes@gmail.com>
2026-05-25 22:53:54 +0530

    StateManagement.jsx

--- StateManagement/Store/Atoms/Count1.jsx
+++
@@ -1,9 +0,0 @@
-import { atom } from "recoil";
-
-
-export const countAtom = atom({           /* Atom act as state variable. */
-
-  key: "countAtom",                       /* Here atom stores an object, here key is for uniqueness. */
-  default: 0
-
-});
